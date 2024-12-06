f57dae3c30488cd6cb70b1f17f2dccf5ad9f09fc
storvik <petterstorvik@gmail.com>
2024-12-06 23:01:35 +0100

    qt-application: fix src

--- qt-application/flake.nix
+++ qt-application/flake.nix
@@ -29,7 +29,7 @@
               name = "myqtapp";
               version = "0.0.1";
 
-              src = gitignoreSource ./.;
+              src = lib.cleanSource ./.;
 
               nativeBuildInputs = [
                 cmake
